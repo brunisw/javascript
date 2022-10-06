3abb7cb205f73f1ea819501acf859d5684f760c8
Bru <bruninharoberta1@hotmail.com>
2022-10-06 18:22:20 +0000

    .

---
+++ Lista javascript marlon/novo.js
@@ -0,0 +1,7 @@
+//5º questão
+const pesoArg = pegaPesoArg();
+const pegaPesoArg1 =  0.037
+const pesoformado = parseFloat(pesoArg);
+const novoValor = (pesoArg * pegaPesoArg1/100) + novoValor;
+console.log("valor é: "+ novoValor);
+
