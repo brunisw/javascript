a66a1ac2546e7c2f641f0351d8a5b94b2cdf34dd
Bru <bruninharoberta1@hotmail.com>
2022-10-13 13:55:29 +0000

    aula de poo

--- aula de leopoldo/aaaa.java
+++ aula de leopoldo/aaaa.java
@@ -1,3 +1,18 @@
-public class minhainterface{
-    public void meumetodoabstrato();
+package forma_geometrica;
+
+public class Objetos_geometricos{
+    //Atributos
+    //1 - Modificador de acesso, 2 - Tipo, 3 - Nome
+    public double altura, comprimento;
+    //Métodos
+    //1 - Modificador de acesso, 2 - Tipo, 3 - Nome
+    public void calcular(){
+
+        //Quadrado
+    resultado = Math.pow(altura, 2);
+        //Triângulo
+    resultado = (altura * comprimento)/2;
+        //Retângulo
+    resultado = altura * comprimento;    
+    }
 }
