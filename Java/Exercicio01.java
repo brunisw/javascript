400f70257e5c371c5d1354e24a4421bb1b1497c6
Bru <bruninharoberta1@hotmail.com>
2022-10-31 11:42:51 +0000

    exercicio novo de java

---
+++ Java/Exercicio01.java
@@ -0,0 +1,17 @@
+package Java;
+
+
+public class Exercicio01 {
+    
+    public static void main(String[] args) {
+        String nome = "Bruna Roberta";
+        int idade = 51;
+        char sexo = 'F';
+        double temperatura = 29.6;
+        boolean arCondicionado = false;
+        System.out.printf("Nome: %s\nIdade: %d\nSexo: %s\nTemperatura: %.1f\nArCondicionado: %b\n", nome, idade, sexo, temperatura, arCondicionado);
+        
+        
+    }
+    
+}
